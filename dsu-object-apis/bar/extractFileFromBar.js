c2a2bd5ae5eb5840289d3341eb22fd68a4575204
skutner <sava.dumitru.daniel@gmail.com>
2023-10-30 18:03:10 +0200

    axiologic-pla/dev##611 allowing for some errors to be redirected to stdout fixes

--- dsu-object-apis/bar/extractFileFromBar.js
+++ dsu-object-apis/bar/extractFileFromBar.js
@@ -6,114 +6,33 @@ const path = require("path");
 
 const tir = require("../../../psknode/tests/util/tir.js");
 
-let folderPath;
-let filePath;
-let barPath;
-
-let folders;
-
-let tempFolder;
-
-const text = ["first text", "second fragment", "third"];
-
-
-let extractFileFromBarTest = {
-    start: function (callback) {
-        this.callback = callback;
-
-        double_check.ensureFilesExist(folders, filePath, text, (err) => {
-            assert.true(err === null || typeof err === "undefined", "Failed to create folder hierarchy.");
-
-            double_check.computeFileHash(filePath, (err, initialHash) => {
-                assert.true(err === null || typeof err === "undefined", "Failed to compute folder hashes.");
-
-                this.initialHash = initialHash;
-                double_check.createTestFolder('AddFilesBatch', async (err, folder) => {
-                    tir.launchApiHubTestNode(100, folder, async err => {
-                        assert.true(err === null || typeof err === "undefined", "Failed to create server.");
-
-                        this.createArchive();
-                    });
-                });
-            });
-        });
-
-    },
-
-    createArchive: function () {
+assert.callback("AddFileEDFSTest", (callback) => {
+    double_check.createTestFolder("extract", async (err, testFolder) => {
+        const vaultDomainConfig = {
+            "anchoring": {
+                "type": "FS",
+                "option": {}
+            }
+        }
+        await tir.launchConfigurableApiHubTestNodeAsync({domains: [{name: "vault", config: vaultDomainConfig}], rootFolder: testFolder});
+        const fs = require("fs");
+        const folderPath = path.join(testFolder, "bar");
+        fs.mkdirSync(folderPath, {recursive: true});
+        const fileContent = "some text";
         const openDSU = require("opendsu");
-        const resolver = openDSU.loadApi("resolver");
-        const keySSISpace = openDSU.loadApi("keyssi");
-
-        resolver.createDSU(keySSISpace.createTemplateSeedSSI("default"), (err, bar) => {
-            if (err) {
-                throw err;
-            }
-
-            this.archive = bar;
-            this.addFile();
-        })
-    },
-
-    addFile: function () {
-        this.archive.safeBeginBatch(err => {
-            if (err) {
-                throw err;
-            }
-            this.archive.addFile(filePath, barPath, async (err, mapDigest) => {
-                if (err) {
-                    throw err;
-                }
-
-                await this.archive.commitBatchAsync();
-                let fs = require("fs");
-                //double_check.deleteFoldersSync(folderPath);
-                fs.rmdirSync(folderPath, {recursive: true, maxRetries: 10});
-                this.archive.getKeySSIAsString((err, keySSI) => {
-                    if (err) {
-                        throw err;
-                    }
-
-                    this.extractFile(keySSI);
-                });
-            });
-        });
-    },
-
-    extractFile: function (keySSI) {
-        const openDSU = require("opendsu");
-        const resolver = openDSU.loadApi("resolver");
-
-        resolver.loadDSU(keySSI, (err, archive) => {
-            if (err) {
-                throw err;
-            }
-
-            archive.extractFile(filePath, barPath, (err) => {
-                if (err) {
-                    throw err;
-                }
-
-                double_check.computeFileHash(filePath, (err, newHash) => {
-                    assert.true(err === null || typeof err === "undefined", "Failed to compute folder hashes.");
-                    assert.true(this.initialHash === newHash, "The extracted file is not te same as the initial one");
-
-                    double_check.deleteFoldersSync([folderPath]);
-
-                    this.callback();
-                });
-            });
-        })
-    }
-};
-
-double_check.createTestFolder("extract", (err, testFolder) => {
-    tempFolder = path.join(testFolder, "tmp");
-    folderPath = path.join(testFolder, "fld");
-    barPath = "myFile";
-    folders = [folderPath];
-    filePath = path.join(testFolder, "fld", "a.txt");
-    assert.callback("AddFileEDFSTest", (callback) => {
-        extractFileFromBarTest.start(callback);
-    }, 20000);
-});
+        const resolver = openDSU.loadAPI("resolver");
+        const keySSISpace = openDSU.loadAPI("keyssi");
+        const filePath = "/file.txt";
+        const dsuInstance = await $$.promisify(resolver.createSeedDSU)("vault");
+        const batchId = await dsuInstance.startOrAttachBatchAsync();
+        await $$.promisify(dsuInstance.writeFile)(filePath, fileContent);
+        await dsuInstance.commitBatchAsync(batchId);
+        const keySSI = await $$.promisify(dsuInstance.getKeySSIAsObject)();
+        const newDSUInstance = await $$.promisify(resolver.loadDSU)(keySSI);
+        const fsFilePath = path.join(folderPath, "file.txt");
+        await $$.promisify(newDSUInstance.extractFile)(fsFilePath, filePath);
+        const extractedFileContent = fs.readFileSync(fsFilePath).toString();
+        assert.true(fileContent === extractedFileContent);
+        callback();
+    });
+}, 20000);
